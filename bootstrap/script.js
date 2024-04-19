dc873639f8f4952dda056f7e4af967e28b424462
JeanJoseph128 <157069796+JeanJoseph128@users.noreply.github.com>
2024-04-19 20:08:55 -0300

    Add files via upload

---
+++ bootstrap/script.js
@@ -0,0 +1,9 @@
+const inputcheck = document.querySelector('#modo-noturno')
+const elemento = document.querySelector('body')
+
+
+
+inputcheck.addEventListener('click', () => {
+    const modo = inputcheck.checked ? 'dark' : 'light'
+    elemento.setAttribute("data-bs-theme", modo)
+})
